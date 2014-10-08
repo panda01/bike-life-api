ba6b49fd864313acf54aa8c5ff394fff624c1a07
Jason Schapiro <yono38@gmail.com>
2014-10-08 15:38:22 -0400

    fix

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,7 @@ class Store < ActiveRecord::Base
   include Foursquare2 
 
   def initialize(client_id, client_secret)
-    self.client = Foursquare2::Client.new(:client_id => FOURSQUARE.client_id, :client_secret => FOURSQUARE.client_secret)
+    self.client = Foursquare2::Client.new(:client_id => FOURSQUARE['client_id'], :client_secret => FOURSQUARE['client_secret'])
   end
 
   def findNearbyShops(ne, sw)
